6e0eb661176990ca2e5c63bb0cc240eabd6e4fbb
Navyakamal <158019137+Navyakamal@users.noreply.github.com>
2024-03-21 22:19:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function display(n){
+    result.value+=n
+}
+function allClear(){
+    result.value=" "
+}
+function backSpace(){
+    result.value=result.value.slice(0,-1)
+}
+function findout(){
+    result.value=eval(result.value)
+}
